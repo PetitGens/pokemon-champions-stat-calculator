8dc267aedf5b9d1ca341670d550b32522bc61985
Julien Ait Azzouzene <julien.azzo27@gmail.com>
2026-04-18 10:41:00 +0200

    refactor: move App component to its own file

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <header>
+      <h1>Stats converter for Pokémon Champions</h1>
+		</header>
+	);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,4 @@
 import { render } from 'preact';
-
-export function App() {
-  return (
-    <header>
-      <h1>Stats converter for Pokémon Champions</h1>
-		</header>
-	);
-}
+import { App } from './components/App';
 
 render(<App />, document.getElementById('app'));
